be6c6fd28fc664d31a62ebab04909317806becab
Bakytzhan Abdurakhim <bakytzhanabdurakhim@gmail.com>
2025-05-11 02:58:37 +0500

    feat(client): add global styles for responsive container and update index to include SCSS

--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app';
 import './index.css';
+import './globals.scss';
 
 const reactRoot = createRoot(document.getElementById('root') as HTMLElement);
 

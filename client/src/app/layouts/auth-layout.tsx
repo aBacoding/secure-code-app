7ce3f9807ee23b07d02f6ab26031fdaa5310ffdd
Bakytzhan Abdurakhim <bakytzhanabdurakhim@gmail.com>
2025-05-12 16:14:05 +0500

    feat(client): update AuthLayout to include AUTH_ROUTES for improved navigation handling; add AUTH_ROUTES configuration for sign-in and sign-up paths

--- client/src/app/layouts/auth-layout.tsx
+++ client/src/app/layouts/auth-layout.tsx
@@ -2,12 +2,13 @@ import { useAuthStore } from '@/features/auth/shared';
 import React, { type FC, type ReactElement } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import Cookies from 'js-cookie';
+import { AUTH_ROUTES } from '@/shared/config';
 
 export const AuthLayout: FC = (): ReactElement => {
   const token = Cookies.get('token');
   const { isAuthenticated } = useAuthStore();
 
-  if (isAuthenticated && token) {
+  if (isAuthenticated && token && AUTH_ROUTES.includes(window.location.pathname)) {
     return <Navigate to="/" />;
   }
 
--- client/src/shared/config/index.ts
+++ client/src/shared/config/index.ts
@@ -1,2 +1,3 @@
 export * from './axios';
 export * from './http';
+export * from './app';
